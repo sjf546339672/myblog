512234573ac2a120b72216fe02e81ea0ef251cd2
shijf <shijf@broada.com>
2020-09-27 14:58:53 +0800

    aaaa

--- train.py
+++ train.py
@@ -1,1 +1,20 @@
 # coding: utf-8
+
+class Test(object):
+
+    # def __init__(self, a, b):
+    #     self.a = a
+    #     self.b = b
+    #
+    # def test(self):
+    #     print("{}的分数是{}".format(self.a, self.b))
+
+    def __call__(self, a):
+        print("计算工资")
+        s = a*12
+        d = a//22.5
+        return dict(s=s, d=d)
+
+
+a = Test()
+print(a(24))
